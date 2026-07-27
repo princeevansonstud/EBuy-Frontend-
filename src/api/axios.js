de52d3f2c49bf6e7efd5b5c7cd75c5e59dc5f42b
prince <prince.evanson@student.moringaschool@gmail.com>
2026-07-27 09:28:12 +0300

    Update Backend Target url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://ebuy-backend.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://https://ebuy-backend-latest.onrender.com/api';
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
